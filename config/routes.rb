df0a80cff03c76e583fe0659aaf629e94cf5a35a
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2023-10-26 15:57:50 +0000

    投稿機機能一覧表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'books/new'
-  post 'books' => 'books#create'
+  post '/books' => 'books#create', as: 'create_book'
   get '/top' => 'homes#top'
   get 'books/:id' => 'books#show',as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'books' => 'books#index'
 end
