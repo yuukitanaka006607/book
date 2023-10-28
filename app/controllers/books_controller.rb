990023a5a478eeb6cfa7075ed7d7d3f7c7c69783
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2023-10-28 08:22:57 +0000

    フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,13 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def create
     book = Book.new(book_params)
     book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(book.id)
   end
   
@@ -26,6 +28,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = 'Book was successfully updated.'
     redirect_to book_path(book.id)
   end
   
