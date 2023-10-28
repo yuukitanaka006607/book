a797fa6553323f79601768a3838e6eff0ae21448
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2023-10-28 09:58:55 +0000

    [Finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,11 +11,15 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
     flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book)
+  else
+   render :index
   end
+end
   
   def show
     @book = Book.find(params[:id])
@@ -26,10 +30,14 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @books = Book.all
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice] = 'Book was successfully updated.'
     redirect_to book_path(book.id)
+  else
+    render :edit
+   end
   end
   
   def destroy
